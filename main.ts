1f02f7edbdbb9c90500a3d1317b29b07c00983fb
cccsy12 <165136960+cccsy12@users.noreply.github.com>
2024-08-05 15:29:30 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+controller.combos.attachCombo("", function () {
+	
+})
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     controller.combos.detachCombo("")
 })
+music.play(music.stringPlayable("A - - - - - G - ", 120), music.PlaybackMode.UntilDone)
