8915718072bdfc0994f7677c3a4e2bce2cddf4d5
websalad123 <tastywebsalad@gmail.com>
2024-08-05 15:31:08 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     controller.combos.detachCombo("")
 })
+controller.player2.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function () {
+    music.play(music.createSong(assets.song`BGM`), music.PlaybackMode.UntilDone)
+})
+forever(function () {
+    music.play(music.stringPlayable("- E - - G A - - ", 120), music.PlaybackMode.UntilDone)
+})
